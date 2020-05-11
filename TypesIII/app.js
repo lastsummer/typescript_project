f0e75a046fc0db28ac0530bbeb0be33476550595
tinghung.wang[王庭歡] <tinghung.wang@tinghungwang.local>
2020-05-11 16:31:32 +0800

    加上function type

--- TypesIII/app.js
+++ TypesIII/app.js
@@ -4,4 +4,20 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function testUndefined(num) {
+    console.log('Result: ' + num);
+    return;
+}
 printResult(add(5, 12));
+var addFun;
+addFun = printResult;
+var combineValue;
+combineValue = printResult;
+combineValue(2);
+function addAndHandel(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandel(5, 6, function (num) {
+    console.log(num);
+});
--- TypesIII/app.ts
+++ TypesIII/app.ts
@@ -6,4 +6,28 @@ function printResult(num: number): void {
   console.log('Result: ' + num)
 }
 
+function testUndefined(num: number):undefined{
+  console.log('Result: ' + num)
+  return
+}
+
 printResult(add(5,12))
+
+let addFun: Function
+addFun = printResult
+
+
+let combineValue: (a: number) => void
+combineValue = printResult
+
+
+combineValue(2)
+
+function addAndHandel(n1:number, n2:number, cb: (num: number)=> void){
+  const result = n1 + n2
+  cb(result)
+}
+
+addAndHandel(5,6, (num) => {
+  console.log(num)
+})
