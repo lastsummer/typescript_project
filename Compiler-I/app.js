ff7a57fc269c8105ed05aa01c8f84dc9c5f0989a
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-05-20 14:08:57 +0800

    新增Compiler folder

--- Compiler-I/app.js
+++ Compiler-I/app.js
@@ -1,3 +1,8 @@
 "use strict";
-var a = "b";
-var b = "c";
+const a = "a";
+const b = "c";
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
+//# sourceMappingURL=app.js.map
--- Compiler-I/app.ts
+++ Compiler-I/app.ts
@@ -1,2 +1,6 @@
-const a = "b"
+const a = "a"
 const b = "c"
+const button = document.querySelector('button')!
+button.addEventListener('click',()=>{
+  console.log('Clicked!')
+});
