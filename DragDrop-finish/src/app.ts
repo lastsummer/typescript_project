aed64fb783cee3d595efc59084e28d7eb460743d
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-09-21 14:34:25 +0800

    新增validate

--- DragDrop-finish/src/app.ts
+++ DragDrop-finish/src/app.ts
@@ -1,1 +1,130 @@
-// Code goes here!
+//Validation
+interface Validatable{
+  value: string | number
+  required?: boolean
+  minLength?: number
+  maxLength?: number
+  min?: number
+  max?: number
+}
+
+function validate(validatableInput: Validatable){
+  let isValid = true
+  if(validatableInput.required){
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0
+  }
+  if(validatableInput.minLength != null && typeof validatableInput.value === 'string'){
+    isValid = isValid && validatableInput.value.length >= validatableInput.minLength
+  }
+
+  if(validatableInput.maxLength != null && typeof validatableInput.value === 'string'){
+    isValid = isValid && validatableInput.value.length <= validatableInput.maxLength
+  }
+
+  if(validatableInput.min != null && typeof validatableInput.value === 'number'){
+    isValid = isValid && validatableInput.value >= validatableInput.min
+  }
+
+  if(validatableInput.max != null && typeof validatableInput.value === 'number'){
+    isValid = isValid && validatableInput.value <= validatableInput.max
+  }
+
+  return isValid
+}
+
+// autobind decorator
+function autobind(_:any, _2: string, decsriptor: PropertyDescriptor){
+  const originalMethod = decsriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get(){
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    }
+  }
+  return adjDescriptor
+}
+
+class ProjectInput {
+  templateElement:  HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLFormElement
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
+
+  constructor(){
+    this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
+    this.hostElement = <HTMLDivElement>document.getElementById('app')!
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = <HTMLFormElement>importedNode.firstElementChild
+    this.element.id = "user-input"
+
+    this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title')
+    this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description')
+    this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people')
+    this.configure()
+    this.attach()
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value
+    const enteredDescription = this.descriptionInputElement.value
+    const enteredPeople = this.peopleInputElement.value
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true
+    }
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    }
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 0,
+      max: 10
+    }
+
+    if(!validate(titleValidatable) ||
+       !validate(descriptionValidatable) ||
+       !validate(peopleValidatable)
+    ){
+      alert('Invalid input, please try again!')
+    }else{
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+  }
+
+  private clearInputs(){
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+
+  @autobind
+  private submitHandler(event: Event){
+    event.preventDefault()
+    const userInput = this.gatherUserInput()
+    if(Array.isArray(userInput)){
+      const [title, desc, people] = userInput
+      console.log(title, desc, people)
+      this.clearInputs()
+    }
+  }
+
+  private configure(){
+    this.element.addEventListener('submit', this.submitHandler)
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  }
+
+}
+
+const prjInput = new ProjectInput();
