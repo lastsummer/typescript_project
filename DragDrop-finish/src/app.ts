34b0ba02ae6987bef98eeee0103175b67fec2070
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-09-29 17:40:29 +0800

    finish drag & drop

--- DragDrop-finish/src/app.ts
+++ DragDrop-finish/src/app.ts
@@ -1,14 +1,41 @@
-// Project State Management
-class ProjectState {
-  private listeners: any[] = []
-  private projects: any[] = []
-  private static instance: ProjectState
-  private constructor(){}
+// Drag & Drop Interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void
+  dragEndHandler(event: DragEvent): void
+}
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void
+  dropHandler(event: DragEvent): void
+  dragLeaveHandler(event: DragEvent): void
+}
 
-  addListener(listenerFn: Function){
+
+// Project class
+enum ProjectStatus { Active, Finished}
+class Project {
+  constructor(public id:string, 
+    public title:string,
+    public description:string,
+    public people:number,
+    public status: ProjectStatus){
+  }
+}
+// Project State Management
+type Listener<T> = (items: T[]) => void
+
+class State<T>{
+  protected listeners: Listener<T>[] = []
+  addListener(listenerFn: Listener<T>){
     this.listeners.push(listenerFn)
   }
+}
 
+class ProjectState extends State<Project>{
+  private projects: Project[] = []
+  private static instance: ProjectState
+  private constructor(){
+    super()
+  }
   static getInstance(){
     if(this.instance){
       return this.instance
@@ -18,13 +45,26 @@ class ProjectState {
   }
 
   addProject(title:string, description: string, numOfPeople: number){
-    const newProject = {
-      id: Math.random().toString(),
+    const newProject = new Project(
+      Math.random().toString(),
       title,
       description,
-      people: numOfPeople
-    }
+      numOfPeople,
+      ProjectStatus.Active
+    )
     this.projects.push(newProject)
+    this.updateListeners()
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus){
+    const project = this.projects.find(prj => prj.id === projectId)
+    if(project && project.status !== newStatus){
+      project.status = newStatus
+    }
+    this.updateListeners()
+  } 
+
+  private updateListeners(){
     for(const listenerFn of this.listeners){
       listenerFn(this.projects.slice())
     }
@@ -80,92 +120,146 @@ function autobind(_:any, _2: string, decsriptor: PropertyDescriptor){
   return adjDescriptor
 }
 
-class ProjectList {
+// Component Base Class
+abstract class Component<T extends HTMLElement, U extends HTMLElement>{
   templateElement:  HTMLTemplateElement
-  hostElement: HTMLDivElement
-  element: HTMLElement
-  assignedProjects: any[]
-
-  constructor(private type: 'active' | 'finished'){
-    this.templateElement = <HTMLTemplateElement>document.getElementById('project-list')!
-    this.hostElement = <HTMLDivElement>document.getElementById('app')!
-    this.assignedProjects = []
+  hostElement: T
+  element: U
+  constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string, ){
+    this.templateElement = <HTMLTemplateElement>document.getElementById(templateId)!
+    this.hostElement = <T>document.getElementById(hostElementId)!
     const importedNode = document.importNode(this.templateElement.content, true)
-    this.element = <HTMLElement>importedNode.firstElementChild
-    this.element.id = `${this.type}-projects`
-    projectState.addListener((projects: any[]) => {
-      this.assignedProjects = projects
-      this.renderProjects()
-    })
-    this.attach()
+    this.element = <U>importedNode.firstElementChild
+    if(newElementId) this.element.id = newElementId
+    this.attach(insertAtStart)
+  }
+  private attach(insertAtBeinning: boolean){
+    this.hostElement.insertAdjacentElement(insertAtBeinning ? 'afterbegin' : 'beforeend', this.element)
+  }
+  abstract configure(): void
+  abstract renderContent(): void
+}
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable{
+  private project: Project
+  get persons(){
+    return this.project.people === 1 ? '1 person' : `${this.project.people} persons`
+  }
+  constructor(hostId: string, project: Project){
+    super('single-project', hostId, false, project.id)
+    this.project = project
+    this.configure()
     this.renderContent()
+  }
+  @autobind
+  dragStartHandler(event: DragEvent){
+    event.dataTransfer!.setData('text/plain', this.project.id)
+    event.dataTransfer!.effectAllowed = 'move'
+  }
+  @autobind
+  dragEndHandler(_: DragEvent){
+    console.log('DragEnd')
+  }
+  configure(){
+    this.element.addEventListener('dragstart', this.dragStartHandler)
+    this.element.addEventListener('dragend', this.dragEndHandler)
+  }
+  renderContent(){
+    this.element.querySelector('h2')!.textContent = this.project.title
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
+    this.element.querySelector('p')!.textContent = this.project.description
+  }
+}
+
+class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget{
+  assignedProjects: Project[]
+  constructor(private type: 'active' | 'finished'){
+    super('project-list', 'app', false, `${type}-projects`)
+    this.assignedProjects = []
+    this.configure()
+    this.renderContent()
+  }
+  @autobind
+  dragOverHandler(event: DragEvent){
+    if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain'){
+      event.preventDefault()
+      const listEl = this.element.querySelector('ul')
+      listEl?.classList.add('droppable')
+    }
+  }
+  @autobind
+  dropHandler(event: DragEvent){
+    const prjId = event.dataTransfer!.getData('text/plain')
+    projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished)
+  }
+  @autobind
+  dragLeaveHandler(_: DragEvent){
+    const listEl = this.element.querySelector('ul')
+    listEl?.classList.remove('droppable')
   }
 
   private renderProjects(){
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement
+    listEl.innerHTML = ''
     for(const prjItem of this.assignedProjects){
-      const listItem = document.createElement('li')
-      listItem.textContent = prjItem.title
-      listEl.appendChild(listItem)
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
     }
   }
-
-  private renderContent(){
+  renderContent(){
     const listId = `${this.type}-projects-list`
     this.element.querySelector('ul')!.id = listId
     this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'
   }
-
-  private attach(){
-    this.hostElement.insertAdjacentElement('beforeend', this.element)
+  configure(){
+    this.element.addEventListener('dragover', this.dragOverHandler)
+    this.element.addEventListener('drop', this.dropHandler)
+    this.element.addEventListener('dragleave', this.dragLeaveHandler)
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter(prj => {
+        if(this.type === 'active'){
+          return prj.status === ProjectStatus.Active
+        }
+        return prj.status === ProjectStatus.Finished
+      })
+      this.assignedProjects = relevantProjects
+      this.renderProjects()
+    })
   }
 }
 
-class ProjectInput {
-  templateElement:  HTMLTemplateElement
-  hostElement: HTMLDivElement
-  element: HTMLFormElement
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
   titleInputElement: HTMLInputElement
   descriptionInputElement: HTMLInputElement
   peopleInputElement: HTMLInputElement
-
   constructor(){
-    this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
-    this.hostElement = <HTMLDivElement>document.getElementById('app')!
-    const importedNode = document.importNode(this.templateElement.content, true)
-    this.element = <HTMLFormElement>importedNode.firstElementChild
-    this.element.id = "user-input"
-
+    super('project-input', 'app', true,  "user-input")
     this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title')
     this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description')
     this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people')
     this.configure()
-    this.attach()
   }
-
+  public configure(){
+    this.element.addEventListener('submit', this.submitHandler)
+  }
   private gatherUserInput(): [string, string, number] | void {
     const enteredTitle = this.titleInputElement.value
     const enteredDescription = this.descriptionInputElement.value
     const enteredPeople = this.peopleInputElement.value
-
     const titleValidatable: Validatable = {
       value: enteredTitle,
       required: true
     }
-
     const descriptionValidatable: Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5
     }
-
     const peopleValidatable: Validatable = {
       value: +enteredPeople,
       required: true,
       min: 0,
       max: 10
     }
-
     if(!validate(titleValidatable) ||
        !validate(descriptionValidatable) ||
        !validate(peopleValidatable)
@@ -175,13 +269,11 @@ class ProjectInput {
       return [enteredTitle, enteredDescription, +enteredPeople]
     }
   }
-
   private clearInputs(){
     this.titleInputElement.value = ''
     this.descriptionInputElement.value = ''
     this.peopleInputElement.value = ''
   }
-
   @autobind
   private submitHandler(event: Event){
     event.preventDefault()
@@ -192,15 +284,7 @@ class ProjectInput {
       this.clearInputs()
     }
   }
-
-  private configure(){
-    this.element.addEventListener('submit', this.submitHandler)
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element)
-  }
-
+  renderContent(){}
 }
 
 const prjInput = new ProjectInput()
--- aa.ts
+++ aa.ts
@@ -1,1 +1,114 @@
-const names: Array<string> = []
+01/06 Javascript — Execution contexts  (7)
+01/07 Javascript — Asynchronous
+01/10 Javascript — Types and Operators
+01/14 Travis — CI
+01/16 Javascript — Object and Function
+01/17 Javascript — by value, by reference
+01/31 MIS — 伺服器大概介紹
+02/03 Javascript — Array、Argument、Semicolon         (18)
+02/05 Javascript — IIFE
+02/06 Javascript — closures 閉包
+02/10 Javascript — this、call()、apply()、bind()
+02/11 MIS — 網路概論- OSI、MAC、IP
+02/13 MIS — Router、ARP、TCP、UDP
+02/14 Javascript — inheritance、prototype、reflection
+02/15 財富自由之路
+02/17 Javascript — function construct、Object.create、class
+02/18 MIS — 架站前的知識
+02/19 AWS — EC2 Basics
+02/20 Javascript — typeof、instanceof、strict mode、method chaining
+02/24 NodeJs — V8: Javascript Engine
+02/25 AWS — AMI
+02/26 MIS — SSO、SIP、SMTP、POP3伺服器
+02/26 NodeJs — Node Core
+02/27 MIS — DHCP伺服器、DNS伺服器
+02/27 Webpack — Loaders、Babel
+03/02 NodeJs — Modules、Exports、Require                   (16)
+03/04 財富的幾何學
+03/05 NodeJs — Module Patterns、exports vs module.exports
+03/05 AWS — EC2 instance
+03/06 MIS — SSL 伺服器
+03/09 MIS — 對外營運伺服器
+03/10 NodeJs — Events and Event Emitter
+03/12 NodeJs — libuv
+03/16 MIS — 預防伺服器發生故障
+03/23 NodeJs — Buffer、Stream
+03/24 AWS — EC2 Security
+03/25 MIS — 伺服器的資安防護
+03/25 NodeJs — File、fs
+03/25 Encode vs Crypto
+03/30 MIS — 伺服器的維運管理
+03/30 NodeJs — HTTP、Web server
+==============41==============
+04/06 NodeJs — server output format               (9)
+04/07 筆記 — 散戶投資上手的第一本書(上)
+04/16 NodeJs — Express(上)
+04/22 NodeJs — Express(下)
+04/24 TypeScript — Getting started
+04/27 Webpack — Plugins
+04/29 Webpack — Dev vs Prod build
+04/30 TypeScript — Types(上)
+04/30 筆記 — 散戶投資上手的第一本書(下)
+05/06 TypeScript — Types(中)                      (15)
+05/06 AWS — Elastic Load Balancing(ELB) — (上)
+05/08 Webpack — Multiple Page Application
+05/11 TypeScript — Types(下)
+05/12 AWS — Elastic Load Balancing(ELB) — (下)
+05/14 Webpack — css font、ESLint
+05/19 TypeScript — Compiler(上)
+05/19 AWS — Auto Scaling Groups — (上)
+05/20 AWS — Auto Scaling Groups — (下)
+05/21 VueJS — first project
+05/21 TypeScript — Compiler(下)
+05/26 AWS — EBS
+05/27 VueJS — Vue instance
+05/28 Docker — init
+05/29 Nuxt — start
+06/01 TypeScript with Next-generation Javascript           (16)
+06/01 AWS — Chat Application — S3
+06/02 Vue JS— Vue instance Lifecycle
+06/02 Docker — Container(上)
+06/03 Docker — Container(下)
+06/04 Nuxt — Pages/Routing/Views
+06/04 TypeScript — Classes(上)
+06/09 AWS — Chat Application — Lambda、IAM
+06/09 AWS — Chat Application — API Gateway — proxy mode
+06/10 Vue JS — development
+06/11 Docker — Create Dockerfile
+06/15 Nuxt — blog project(static)
+06/16 TypeScript — Classes(下) — Inheritance
+06/19 AWS — Chat Application — DynamoDB
+06/20 Vue JS — Components
+06/23 Docker — Create NodeJS project
+==============40==============
+07/01 Nuxt — Handing Data                       (12)
+07/01 TypeScript — Interface
+07/03 AWS — Chat Application — API Gateway — mapping modes(上)
+07/07 AWS — Chat Application — API Gateway — mapping modes(下)
+07/09 VueJS — Component communicating
+07/14 Docker — Compose
+07/17 Nuxt — Backend
+07/20 TypeScript — Advance Types(上)
+07/21 TypeScript — Advance Types(下)
+07/22 AWS — Chat Application — Cognito(上)
+07/23 AWS — Chat Application — Cognito(下)
+07/28 VueJS — Advanced Component — Slot
+08/03 Docker — Create Web Project               (7)
+08/06 Nuxt — Config
+08/11 Typescript — Generic
+08/18 AWS — Chat Application — Lambda version、CloudFront
+08/19 VueJS — Second Project
+08/21 Docker — CI/CD
+08/26 Nuxt — Middleware & Authentication
+09/01 Typescript — Decorators(上)               (12)
+09/02 Typescript — Decorators(下)
+09/08 AWS — Codepipeline(上)
+09/09 AWS — Codepipeline(下)
+09/10 VueJS — User Input with Forms
+09/15 Docker — Multiple Container(上)
+09/16 Nuxt — SPA
+09/21 Typescript — Project(上)
+09/22 AWS — CodeCommit
+09/23 VueJS — Directives
+09/26 Docker — Multiple Container(中) — DockerCompose
+09/28 JavaScript — Asynchronous vs synchronous
