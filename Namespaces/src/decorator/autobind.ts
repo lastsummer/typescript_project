ede41a2b5b323a4621642a2c0a707579bb2d955f
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-10-13 17:37:35 +0800

    add Namespaces

---
+++ Namespaces/src/decorator/autobind.ts
@@ -0,0 +1,14 @@
+namespace App{
+  // autobind decorator
+  export function autobind(_:any, _2: string, decsriptor: PropertyDescriptor){
+    const originalMethod = decsriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get(){
+        const boundFn = originalMethod.bind(this)
+        return boundFn
+      }
+    }
+    return adjDescriptor
+  }
+}
