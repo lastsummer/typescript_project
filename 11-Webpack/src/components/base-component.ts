70f304fcb91c7a15ff67581581e9301702a6baca
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-10-28 11:12:14 +0800

    add webpack

---
+++ 11-Webpack/src/components/base-component.ts
@@ -0,0 +1,20 @@
+
+// Component Base Class
+export abstract class Component<T extends HTMLElement, U extends HTMLElement>{
+  templateElement:  HTMLTemplateElement
+  hostElement: T
+  element: U
+  constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string, ){
+    this.templateElement = <HTMLTemplateElement>document.getElementById(templateId)!
+    this.hostElement = <T>document.getElementById(hostElementId)!
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = <U>importedNode.firstElementChild
+    if(newElementId) this.element.id = newElementId
+    this.attach(insertAtStart)
+  }
+  private attach(insertAtBeinning: boolean){
+    this.hostElement.insertAdjacentElement(insertAtBeinning ? 'afterbegin' : 'beforeend', this.element)
+  }
+  abstract configure(): void
+  abstract renderContent(): void
+}
---
+++ 11-Webpack/src/components/project-item.ts
@@ -0,0 +1,36 @@
+import { Component } from './base-component.js'
+import { autobind } from '../decorator/autobind.js'
+import { Draggable } from '../models/drag-drop.js'
+import { Project } from '../models/project.js'
+
+
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable{
+  private project: Project
+  get persons(){
+    return this.project.people === 1 ? '1 person' : `${this.project.people} persons`
+  }
+  constructor(hostId: string, project: Project){
+    super('single-project', hostId, false, project.id)
+    this.project = project
+    this.configure()
+    this.renderContent()
+  }
+  @autobind
+  dragStartHandler(event: DragEvent){
+    event.dataTransfer!.setData('text/plain', this.project.id)
+    event.dataTransfer!.effectAllowed = 'move'
+  }
+  @autobind
+  dragEndHandler(_: DragEvent){
+    console.log('DragEnd')
+  }
+  configure(){
+    this.element.addEventListener('dragstart', this.dragStartHandler)
+    this.element.addEventListener('dragend', this.dragEndHandler)
+  }
+  renderContent(){
+    this.element.querySelector('h2')!.textContent = this.project.title
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
+    this.element.querySelector('p')!.textContent = this.project.description
+  }
+}
